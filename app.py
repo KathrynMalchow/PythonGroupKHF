d0edf112d1572c310d7d650b03af17818c4c7c51
Kathryn Malchow <78643030+KathrynMalchow@users.noreply.github.com>
2021-12-15 18:10:16 +0100

    changed font

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import requests
 import json
-
 import tkinter as tk
 from tkinter import *
 from PIL import Image, ImageTk
 
+#start GUI
 root =tk.Tk()
 root.title('Fortune Teller')
 
-canvas = tk.Canvas(root, width=550, height=300)
+#area of GUI
+canvas = tk.Canvas(root, width=600, height=300)
 canvas.grid(columnspan=3, rowspan=3)
-
 
 #logo
 logo = Image.open('icon.png')
@@ -19,27 +19,20 @@ logo_label = tk.Label(image = logo)
 logo_label.image = logo
 logo_label.grid(column=1, row=0)
 
-
-#instructions
-instructions = Label(root, text = "What is your destiny?", font="FinkHeavy")
+#caption
+instructions = Label(root, text = "What is your destiny? Select your Zodiac Sign and Day of Choice", font="Harrington")
 instructions.grid(columnspan=3, column=0, row=1)
-
-#area
-canvas = tk.Canvas(root, width=600, height=200)
-canvas.grid(columnspan=3)
 
 #dropdown list for zodiac sign
 data_sign={
      "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"
 }
 
-
 sign = tk.StringVar()
 sign.set("Zodiac Sign")
 
 s = OptionMenu(root, sign, *data_sign)
 s.grid(column=1, row=2)
-
 
 #dropdown list for day options
 data_day={
@@ -52,6 +45,7 @@ day.set("Day")
 d = OptionMenu(root, day, *data_day)
 d.grid(column=1, row=3)
 
+#function to format response
 def content(json):
     date = json.get('current_date')
     descr = json.get('description')
@@ -64,7 +58,6 @@ def content(json):
     final_sr = 'Horoscope for: %s \nFortune: %s \nCompatibility: %s \nMood: %s \nLucky ''Color: %s \nLucky ''Number: %s \nLucky ''Time: %s' % (date, descr, compat, mood, color, number, time)
 
     return final_sr
-
 
 #use API as function
 def get_horoscope(sign, day):
@@ -90,11 +83,9 @@ def get_horoscope(sign, day):
     text_box.grid(column=1, row=5)
 
 #button
-button = Button(root,  text='Tell Me', bg ='#af7ac5', font=40, command=lambda:get_horoscope(sign.get(), day.get()))
+button = Button(root,  text='Tell Me', bg ='#af7ac5', font="Harrington", height=2, width=10, command=lambda:get_horoscope(sign.get(), day.get()))
 button.grid(column=1,row=4)
 
 
-
-
-
+#end GUI
 root.mainloop()
